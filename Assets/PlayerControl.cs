e02f004bdc9a7a69974830a40988e6fccc5dde05
Arroria <mermerkwon@naver.com>
2019-01-01 17:37:50 +0900

    플레이어가 생겼습니다. 돌아다닐수 있고 마우스를 바라봅니다.

---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+    public float velocity;
+    public Animator animator;
+    public SpriteRenderer spriteRenderer;
+    public GameObject cursor;
+
+	void Start ()
+    {
+        cursor = GameObject.Find("Cursor");
+	}
+	
+	void Update ()
+    {
+        Vector2 movement = new Vector2(0, 0);
+        if (Input.GetKey(KeyCode.A))    movement.x--;
+        if (Input.GetKey(KeyCode.D))    movement.x++;
+        if (Input.GetKey(KeyCode.W))    movement.y++;
+        if (Input.GetKey(KeyCode.S))    movement.y--;
+        movement *= velocity * Time.deltaTime;
+        transform.position += new Vector3(movement.x , movement.y);
+
+        bool lookAtRight = transform.position.x <= cursor.transform.position.x;
+        spriteRenderer.flipX = !lookAtRight;
+
+        animator.SetBool("isMoving", (movement.x != 0) || (0 != movement.y));
+    }
+}
---
+++ Assets/cursor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cursor : MonoBehaviour {
+    
+
+	void Start ()
+    {
+        Update();
+	}
+	
+	void Update ()
+    {
+        Vector3 mPos = Input.mousePosition;
+        mPos.z = 0;
+        transform.position = Camera.main.ScreenToWorldPoint(mPos);
+	}
+}
